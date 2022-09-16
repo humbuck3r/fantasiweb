8175189a22be3ea1c78d14fe9e1cab87f0de45e7
unknown <sr.ska89@gmail.com>
2022-09-15 21:45:37 -0300

    agregamos dependecias y maquetamos algunos componentes

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import "./Services.css";
+import Wsp from '../../Assets/Img/wsp card.png';
+import Cake from '../../Assets/Img/cake card.png';
+import Buy from '../../Assets/Img/buy card.png';
+import Delivery from '../../Assets/Img/delivery card.png';
+
 
 const Services = () => {
     return (
-        <div>
-            <div><h1>serv</h1></div>
+      <div className="serv">
+        <h1>NUESTROS SERVICIOS</h1>
+        <h2>¡Tu cumpleaños o evento como a vos te guste!</h2>
+        <div className="cards-serv">
+          <div className="card">
+            <img src={Wsp} className="wsp-img" />
+            <p>Atención inmediata por Whatsapp</p>
+          </div>
+          <div className="card">
+            <img src={Cake} className="wsp-img" />
+            <p>Atención inmediata por Whatsapp</p>
+          </div>
+          <div className="card">
+            <img src={Buy} className="wsp-img" />
+            <p>Atención inmediata por Whatsapp</p>
+          </div>
+          <div className="card">
+            <img src={Delivery} className="wsp-img" />
+            <p>Atención inmediata por Whatsapp</p>
+          </div>
         </div>
+      </div>
     );
 }
 
--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -1,11 +1,51 @@
-import React from 'react';
+import Carousel from "react-bootstrap/Carousel";
+import "./Slider.css";
+import Slider1 from "../../Assets/Img/slider1.png"
+import Slider2 from "../../Assets/Img/Slider2.png"
+import Slider3 from "../../Assets/Img/Slider3.png"
 
-const Slider = () => {
-    return (
-        <div>
-            <div><h1>Slider</h1></div>
-        </div>
-    );
+function Slider() {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={Slider1}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={Slider2}
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={Slider3}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
 }
 
 export default Slider;
