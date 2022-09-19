d53d8baccbfd3df542c9b6795f320ea6fddfb212
Ezequiel Montero <ezequielmonterodev@gmail.com>
2022-09-19 13:01:50 -0300

    primer commit desde ezedevelop

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,23 +1,20 @@
 import React from "react";
-import Logo from '../../Assets/Img/Fantasy - logo 1.png';
-import './About.css';
+import Logo from "../../Assets/Img/Fantasy - logo 1.png";
+import "./About.css";
 const About = () => {
   return (
-    
-      <div className="about">
-        <img src={Logo} alt=''/>
-        <h1>SOBRE NOSOTROS</h1>
-        <p>
-          Fantasi es una empresa familiar que nace a mediados de 2007.
-          Actualmente cuenta con un local dedicado a la venta de cotillón y
-          repostería. Con un amplio surtido de productos para carnaval,
-          cumpleaños infantiles, bautismos, halloween, pascuas, fiestas
-          navideñas, quinces, etc. A base de mucho esfuerzo y esmero, sigue hoy
-          más vigente que nuncreciendo los mejores precios y la mayor
-          calidad del mercado.
-        </p>
-      </div>
-    
+    <div className="about">
+      <img src={Logo} alt="" />
+      <h1>SOBRE NOSOTROS</h1>
+      <p>
+        Fantasi es una empresa familiar que nace a mediados de 2007. Actualmente
+        cuenta con un local dedicado a la venta de cotillón y repostería. Con un
+        amplio surtido de productos para carnaval, cumpleaños infantiles,
+        bautismos, halloween, pascuas, fiestas navideñas, quinces, etc. A base
+        de mucho esfuerzo y esmero, sigue hoy más vigente que nunca ofreciendo
+        los mejores precios y la mayor calidad del mercado.
+      </p>
+    </div>
   );
 };
 
