7f543eb7b7386aa0478195ab800d189142bee6ee
Ezequiel Montero <ezequielmonterodev@gmail.com>
2022-09-28 15:22:16 -0300

    estilos al navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -5,21 +5,21 @@ import "./Navbar.css"
 
 function HeadNav() {
   return (
-    <Navbar bg="light" expand="lg" className="nav-bar">
+    <Navbar bg="light" expand="lg" className="nav-bar fixed-top">
       
         <Navbar.Brand href="#home">
           <a alt="logo" href="#">
-            <img src={Logo} className="" alt="" />
+            <img src={Logo} className="logo" alt="" />
           </a>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto navs">
-            <Nav.Link href="#home">Inicio</Nav.Link>
-            <Nav.Link href="#link">Servicios</Nav.Link>
-            <Nav.Link href="#link">Productos</Nav.Link>
-            <Nav.Link href="#link">Nosotros</Nav.Link>
-            <Nav.Link href="#link">Contacto</Nav.Link>
+            <Nav.Link href="#home"><h2 className="navs-title">Inicio</h2></Nav.Link>
+            <Nav.Link href="#link"><h2 className="navs-title">Servicios</h2></Nav.Link>
+            <Nav.Link href="#link"><h2 className="navs-title">Productos</h2></Nav.Link>
+            <Nav.Link href="#link"><h2 className="navs-title">Nosotros</h2></Nav.Link>
+            <Nav.Link href="#link"><h2 className="navs-title">Contacto</h2></Nav.Link>
           </Nav>
         </Navbar.Collapse>
         <button className="btn-nav">
