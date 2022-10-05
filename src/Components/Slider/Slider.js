9742ff140838b4826091d6a8ee996893395d907d
unknown <sr.ska89@gmail.com>
2022-10-05 17:11:28 -0300

    cambios en navbar y tipografias

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -14,8 +14,7 @@ function Slider() {
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
@@ -26,8 +25,7 @@ function Slider() {
         />
 
         <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+   
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
@@ -38,10 +36,7 @@ function Slider() {
         />
 
         <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+          
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
