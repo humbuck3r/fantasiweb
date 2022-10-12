cf38b658b0282c8f0623ae3dd59f23b7022f2847
barbi conte <vicky@MacBook-Air-de-maria.local>
2022-10-12 18:41:59 -0300

    cambios en contacto

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Slider from './Components/Slider/Slider';
 import Footer from './Components/Footer/Footer';
-import Maps from './Components/Map/Map';
 import ProdServ from './Components/Prod-serv/Prod-serv';
 import About from './Components/About/About';
 
@@ -14,7 +13,7 @@ function App() {
       <Slider/>
       <ProdServ/>
       <About/>
-      <Maps/>   
+      
       <Footer/>  
     </div>
   );
--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,22 +1,17 @@
 import React from 'react';
-import Map from '../Map/Map';
-import credentials from '../Map/Credentials';
 
-const mapURL = `https://maps.googleapis.com/maps/api/js?v=3.exp&key=${credentials.mapsKey}`;
+
+
 
 
 const Contact = () => {
     return (
         <div className='contact'>
             <div className='title-contact'><h1>visitanos en nuestra tienda</h1></div>
-             <div className='map-contact'>
-             <Map 
-            //googleMapURL = {mapURL}
-            //containerElement= {<div style={{height : '400px'}}/>}
-            //mapElement = {<div style={{height:'100%'}}/>}
-            //loadingElement = {<p>Cargando</p>}
             
-            /></div>
+             
+            
+           
         </div>
     );
 }
