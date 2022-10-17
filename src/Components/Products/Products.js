b8e69355467994b5b8cc214dea0da6755f18af88
Ezequiel Montero <ezequielmonterodev@gmail.com>
2022-10-17 17:34:38 -0300

    cambios en el responsive de desktop

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,34 +1,62 @@
 import React from "react";
 import "./Products.css";
-import Sliderprod from '../Slider-prod/Slider-prod'
+import Carousel from "react-bootstrap/Carousel";
+import Choco from "../../Assets/Img/cupcake.png";
 const Products = () => {
   return (
-      <><h1 className="title-prod">NUESTROS PRODUCTOS</h1><div className="container-prod">
-      <Sliderprod />
-      <div className="subcontainer-prod">
-
-        <div className="content-prod">
-          <p>
-            Contamos con un amplio surtido en productos, abarcando carnaval,
-            cotillón infantil, materias primas y accesorios de repostería,
-            golosinas, manualidades, disfraces, descartables y mucho más.
-            <ul className="content-prod-list">
-              <li>Mejor ecuación Precio / Beneficio para nuestros clientes.</li>
-            </ul>
-            <ul>
-              <li>Innovación constante en nuestro modelo de ventas.</li>
-            </ul>
-          </p>
+    <>
+     
+      
+      <h2 className="title-prod">NUESTROS PRODUCTOS</h2>
+      <div className="container-prod">
+      <div className="carousel-cont">  
+          <Carousel variant="dark" className="slide-prod ">
+            <Carousel.Item className="slide-item">
+              <img
+                className="d-block w-100 img-prod "
+                src={Choco}
+                alt="First slide"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100 img-prod "
+                src={Choco}
+                alt="Second slide"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100 img-prod "
+                src={Choco}
+                alt="Third slide"
+              />
+            </Carousel.Item>
+          </Carousel>
+          </div>
+        <div className="subcontainer-prod">
+          <div className="content-prod">
+            <p>
+              Contamos con un amplio surtido en productos, abarcando carnaval,
+              cotillón infantil, materias primas y accesorios de repostería,
+              golosinas, manualidades, disfraces, descartables y mucho más.
+              <ul className="content-prod-list">
+                <li>
+                  Mejor ecuación Precio / Beneficio para nuestros clientes.
+                </li>
+              </ul>
+              <ul>
+                <li>Innovación constante en nuestro modelo de ventas.</li>
+              </ul>
+            </p>
+          </div>
         </div>
       </div>
-        
-      
-    </div>
-    <button className="btn-prod">
-          <h2>¡Quiero una Cotización!</h2>
-        </button></>
-    
+      <button className="btn-prod">
+        <h2>¡Quiero una Cotización!</h2>
+      </button>
+    </>
   );
-}
+};
 
 export default Products;
