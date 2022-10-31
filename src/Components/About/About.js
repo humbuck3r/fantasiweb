11c9b3db8632678a46dbfdd4f1b08095fb11e994
Ezequiel Montero <ezequielmonterodev@gmail.com>
2022-10-31 11:29:45 -0300

    agregamos links a los botones

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./About.css";
-import iconoubi from '../../Assets/Img/icono-ubi.png';
-import Scroll from '../Scroll-up/Scroll';
+import iconoubi from "../../Assets/Img/icono-ubi.png";
+import Scroll from "../Scroll-up/Scroll";
 const About = () => {
   return (
     <div className="about">
@@ -17,10 +17,16 @@ const About = () => {
           calidad del mercado.
         </p>
         <div className="scroll-container">
-          <button className="btn-about">
+          <a href="https://www.google.com/maps/place/Fantasi+COTILLON/@-32.928982,-68.856196,15z/data=!4m5!3m4!1s0x0:0x6b8cb66bad18e402!8m2!3d-32.928982!4d-68.856196">
+          <button
+            className="btn-about"
+            
+          >
+           
             <img src={iconoubi} className="btn-icono" alt="" />
             <h2>Visitanos en nuestra tienda</h2>
           </button>
+          </a>
           <Scroll />
         </div>
       </div>
