5786d725eec2734f8938b6e6570ed3a2bf6968f4
humbuck3r <sr.ska89@gmail.com>
2023-07-05 13:08:31 -0300

    subo imagenes a slider

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -1,15 +1,15 @@
 import Carousel from "react-bootstrap/Carousel";
 import "./Slider.css";
-import Slider2 from "../../Assets/Img/1.jpg"
-
-
+import Slider1 from "../../Assets/Img/hist.jpg"
+import Slider3 from "../../Assets/Img/SLIDERHORARIO.png"
+import Slider4 from "../../Assets/Img/slidertodo.png"
 function Slider() {
   return (
     <Carousel variant="dark" className="slide" id="home">
       <Carousel.Item>
         <img
           className="d-block w-100 img fluid"
-          src={Slider2}
+          src={Slider1}
           alt="First slide"
         />
        
@@ -17,7 +17,7 @@ function Slider() {
       <Carousel.Item>
         <img
           className="d-block w-100 img fluid"
-          src={Slider2}
+          src={Slider3}
           alt="Second slide"
         />
         
@@ -25,7 +25,7 @@ function Slider() {
       <Carousel.Item>
         <img
           className="d-block w-100 img fluid"
-          src={Slider2}
+          src={Slider4}
           alt="Third slide"
         />
         
